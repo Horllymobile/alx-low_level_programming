5af491f6a053e2228438966e723fccea4afa35e3
Abraham James <horlamidex1@gmail.com>
2022-03-10 14:24:23 -0800

    Fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
 
+/**
+* main - This function is the starting point
+* of the program that print a text to screen
+* Return: 0
+**/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
