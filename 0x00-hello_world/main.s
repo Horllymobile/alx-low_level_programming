855eb76f6fd001724646ee5382d1ede28d9372b1
Abraham James <horlamidex1@gmail.com>
2022-03-10 15:44:07 -0800

    Fixed error

--- 0x00-hello_world/main.s
+++ 0x00-hello_world/main.s
@@ -1,5 +1,30 @@
 	.file	"main.c"
+	.intel_syntax noprefix
 	.text
+	.section	.rodata
+.LC0:
+	.string	"Hello World"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	lea	rdi, .LC0[rip]
+	call	puts@PLT
+	mov	eax, 0
+	pop	rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
 	.ident	"GCC: (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0"
 	.section	.note.GNU-stack,"",@progbits
 	.section	.note.gnu.property,"a"
