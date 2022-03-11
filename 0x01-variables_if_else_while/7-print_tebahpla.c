266c546e8668ff00b53c6f80c24bc7e80b7b1717
Abraham James <horlamidex1@gmail.com>
2022-03-11 15:33:49 -0800

    Fixed

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,14 +2,13 @@
 /**
 * main - starting point of C program
 * Return: number - 1
-0;276;0c **/
+**/
 int main(void)
 {
-int i = 26;
-char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
-for (; i > -1; i--)
+int i;
+for (i = 'z'; i >= 'a'; i--)
 {
-putchar(alpha[i]);
+putchar(i);
 }
 putchar('\n');
 return (0);
