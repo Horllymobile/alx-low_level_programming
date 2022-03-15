6690a1abe7d9743bf152b15990a0196a0d675fbf
Abraham James <horlamidex1@gmail.com>
2022-03-15 16:18:00 -0700

    Fixed

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,8 +9,18 @@
 int print_sign(int n)
 {
 if (n > 0)
+{
+_printchar('+');
 return (1);
+}
 else if (n == 0)
+{
+_printchar('0');
 return (0);
- return (-1);
+}
+else
+{
+_printchar('-');
+return (1);
+}
 }
