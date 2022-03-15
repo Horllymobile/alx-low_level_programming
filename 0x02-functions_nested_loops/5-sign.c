bef3bbe468d84d9939beb13ae1e678161fb8e407
Abraham James <horlamidex1@gmail.com>
2022-03-15 16:09:26 -0700

    Write

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_sign - check for lowercase character
+ * @n: The number to check for
+ *
+ * Return: 1 if @n is lowercase otherwise 0
+ */
+int print_sign(int n)
+{
+if (n > 0)
+return (1);
+else if (n == 0)
+return (0);
+else if (n < 0)
+return (-1);
+}
