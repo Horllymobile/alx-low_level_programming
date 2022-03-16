21ac354c33aa4fd54537291f77ac33b4997b2b5e
Abraham James <horlamidex1@gmail.com>
2022-03-15 23:54:11 -0700

    Fixed

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,12 +12,12 @@ for (h1 = '0'; h1 <= '2'; h1++){
 for (h2 = '0'; h2 <= '3'; h2++){
 for (m1 = '0'; m1 <= '5'; m1++){    
 for (m2 = '0'; m2 <= '9'; m2++){
-putchar(h1);
-putchar(h2);
-putchar(':');
-putchar(m1);
-putchar(m2);
-putchar('\n');
+_putchar(h1);
+_putchar(h2);
+_putchar(':');
+_putchar(m1);
+_putchar(m2);
+_putchar('\n');
 }
 }
 }
