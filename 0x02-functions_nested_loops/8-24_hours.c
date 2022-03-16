edb91cb2332e2efb6f4561549a61c1bd4e57bdc3
Abraham James <horlamidex1@gmail.com>
2022-03-16 00:12:00 -0700

    Fixed issue

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,10 +7,16 @@
 void jack_bauer(void)
 {
 int h1, h2, m1, m2;
-h1 = h2 = m1 = m2 = 0;
+
+h1 = h2 = m1 = m2 = '0';
 for (h1 = '0'; h1 <= '2'; h1++)
 {
-for (h2 = '0'; h2 <= '3'; h2++)
+int l;
+
+l = '9';
+if (h1 == '2')
+l = '3';
+for (h2 = '0'; h2 <= l; h2++)
 {
 for (m1 = '0'; m1 <= '5'; m1++)
 {
