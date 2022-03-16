c2b6e09c83e14fd61479eaba14a93a95e5922431
Abraham James <horlamidex1@gmail.com>
2022-03-15 23:13:17 -0700

    Write

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit - print last digit of a number
+ * @c: and integer parameter
+ * Return: integer
+ **/
+int print_last_digit(int c)
+{
+return c % 10;
+}
