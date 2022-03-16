5a790975f694d14a8deb4d53550f566dee489d5b
Abraham James <horlamidex1@gmail.com>
2022-03-15 23:52:54 -0700

    Write

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of
+ * Jack Bauer, starting from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+int h1, h2, m1, m2;
+h1 = h2 = m1 = m2 = 0;
+for (h1 = '0'; h1 <= '2'; h1++){
+for (h2 = '0'; h2 <= '3'; h2++){
+for (m1 = '0'; m1 <= '5'; m1++){    
+for (m2 = '0'; m2 <= '9'; m2++){
+putchar(h1);
+putchar(h2);
+putchar(':');
+putchar(m1);
+putchar(m2);
+putchar('\n');
+}
+}
+}
+}
+}
