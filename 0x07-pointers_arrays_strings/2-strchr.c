a1ef00171798b7fe8414c8832eb0789a4e088cc2
Abraham James <horlamidex1@gmail.com>
2022-03-28 15:47:23 -0700

    Fixed

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,5 @@
 #include "main.h"
-/**
+0;276;0c/**
  * _strchr - entry point
  * @s: pointe to string
  * @c: character
@@ -8,6 +8,7 @@
 char *_strchr(char *s, char c)
 {
 unsigned int i = 0;
+
 for (; *(s + i) != '\0'; i++)
 {
 if (*(s + i) == c)
@@ -17,5 +18,5 @@ return (s + i);
 }
 if (*(s + i) == c)
 return (s + i);
-return (NULL);
+return ('\0');
 }
