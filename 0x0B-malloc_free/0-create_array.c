784d387f6ea54f690bb5ec6fc79b2887ee68d973
Abraham James <horlamidex1@gmail.com>
2022-04-05 08:49:54 -0700

    Added fix form valigrid

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,6 +3,7 @@
  * create_array - Entry point
  * @size: size of array
  * @c: character to fill array
+ * Return: array of char
  */
 
 char *create_array(unsigned int size, char c)
@@ -16,6 +17,6 @@ array = malloc(size * sizeof(char));
 
 for (i = 0; i < size; i++)
 array[i] = c;
-
+free(array);
 return (array);
 }
