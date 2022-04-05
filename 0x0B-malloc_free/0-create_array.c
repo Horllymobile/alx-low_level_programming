f16ea5de1f3e06429e76d77d13de3e68dc55f4da
Abraham James <horlamidex1@gmail.com>
2022-04-05 08:54:09 -0700

    Fixed

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,18 +5,24 @@
  * @c: character to fill array
  * Return: array of char
  */
-
 char *create_array(unsigned int size, char c)
 {
-char *array;
-unsigned int i;
+char *p;
+unsigned int i = 0;
 if (size == 0)
 return (NULL);
+p = (char *) malloc(sizeof(char) * size);
 
-array = malloc(size * sizeof(char));
+if (p == NULL)
+return (0);
 
-for (i = 0; i < size; i++)
-array[i] = c;
-free(array);
-return (array);
+while (i < size)
+{
+*(p + i) = c;
+i++;
+}
+
+*(p + i) = '\0';
+
+return (p);
 }
