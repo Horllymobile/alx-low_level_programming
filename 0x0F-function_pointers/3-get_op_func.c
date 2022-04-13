3343ce255e1c26b4c5bb6f4861181c5d329092e1
Abraham James <horlamidex1@gmail.com>
2022-04-13 10:51:44 -0700

    Fixed

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,25 +1,29 @@
 #include "3-calc.h"
+
 /**
- * get_op_func - Get the op func object
- * @s: The operator
- * Return: int(*)(int, int)
+ * get_op_func - selects the correct function to perform
+ * the operation asked by the user.
+ * @s: char operator.
+ *
+ * Return: pointer to the function that corresponds to the operator.
  */
 int (*get_op_func(char *s))(int, int)
 {
-int (*f)(int, int);
-if (s == NULL)
-return (NULL);
-if (s[0] == '+')
-f = &op_add;
-else if (s[0] == '-')
-f = &op_sub;
-else if (s[0] == '*')
-f = &op_mul;
-else if (s[0] == '/')
-f = &op_div;
-else if (s[0] == '%')
-f = &op_mod;
-else
-f = NULL;
-return (f);
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i = 0;
+
+while (i < 10)
+{
+if (s[0] == ops->op[i])
+break;
+i++;
+}
+return (ops[i / 2].f);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,16 +1,16 @@
 #include "3-calc.h"
 
 /**
- * main - Entry point
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: int
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: Always 0.
  */
-
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-int (*f)(int, int);
 int a, b;
+int (*operation)(int, int);
 
 if (argc != 4)
 {
@@ -18,22 +18,23 @@ printf("Error\n");
 exit(98);
 }
 
-
-a = atoi(argv[1]);
-b = atoi(argv[3]);
-f = get_op_func(argv[2]);
-
 if (argv[2][1])
 {
 printf("Error\n");
 exit(99);
 }
 
-if (f == NULL)
+operation = get_op_func(argv[2]);
+
+if (operation == NULL)
 {
 printf("Error\n");
 exit(99);
 }
-printf("%d\n", f(a, b));
+
+a = atoi(argv[1]);
+b = atoi(argv[3]);
+
+printf("%d\n", operation(a, b));
 return (0);
 }
