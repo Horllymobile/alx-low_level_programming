ad7f1c15740e1176578cb91481a18d3fead96977
Abraham James <horlamidex1@gmail.com>
2022-04-15 05:50:18 -0700

    Fixed betty

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,11 +1,17 @@
 #include "variadic_functions.h"
 
+/**
+ * sum_them_all - sum all arguments
+ * @n: number of arguments
+ * @...: array of arguments
+ * Return: int
+ */
 int sum_them_all(const unsigned int n, ...)
 {
 va_list valist;
 unsigned int i, sum = 0;
-
-if (n == 0) return (0);
+if (n == 0)
+return (0);
 
 va_start(valist, n);
 for (i = 0; i < n; i++)
