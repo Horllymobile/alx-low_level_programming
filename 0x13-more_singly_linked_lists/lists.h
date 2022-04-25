3f60e162c7feb1b84bbf248890cc8c93fde28171
Abraham James <horlamidex1@gmail.com>
2022-04-25 15:33:46 -0700

    Write a function that adds a new node at the beginning of a listint_t list.

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,6 +18,7 @@ struct listint_s *next;
 /* Custome functioons */
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 
 /* Put char */
 int _putchar(char c);
